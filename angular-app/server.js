b831219e015fdf08644ea2d69d9e8d5e60831656
nalapatt <70668968+nalapatt@users.noreply.github.com>
2021-03-17 19:07:21 -0400

    Update server.js

--- angular-app/server.js
+++ angular-app/server.js
@@ -1,10 +1,15 @@
- const angular = require("angular");
- const app  = angular();
- 
- app.get("/", function(req, res){
-     res.send("Hello from angular!");
- });
+http.get('http://localhost:8080/dowork',{"msg":"hi"}).success(function(data){
+ console.log(data);
+}); 
 
- app.listen(4200, function(){
-     console.log("Projects in Docker API server started at");
- });
+
+ #const angular = require("angular");
+ #const app  = angular();
+ 
+ #app.get("/", function(req, res){
+ #    res.send("Hello from angular!");
+ #});
+
+ #app.listen(4200, function(){
+ #    console.log("Projects in Docker API server started at");
+ #});
